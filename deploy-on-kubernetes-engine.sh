3a19a1476f37cb86cfcdc3d68c6ef6f93832c6e1
Michael Haas <michael.haas@computerlinguist.org>
2018-03-18 09:20:02 +0100

    Extend deploy-on-kubernetes-engine.sh

--- deploy-on-kubernetes-engine.sh
+++ deploy-on-kubernetes-engine.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-#!/bin/bash
-
 source common.sh
 
+#gcloud container clusters get-credentials cluster-1
+
+echo "-- Creating Deployment."
+echo "-- Prerequisite: Docker image for current COMMIT_SHA must be available on gcr.io!"
+
 # kubectl run creates a "Deployment".
-kubectl run tensorflask --image=gcr.io/${PROJECT_ID}/tensorflask --port 8000
+kubectl run tensorflask --image=gcr.io/${PROJECT_ID}/tensorflask:$COMMIT_SHA --port 8000
+
+echo "-- Creating Service to expose Deployment"
+kubectl expose deployment tensorflask --type=LoadBalancer --port 8000 --target-port 8000
+
+echo "-- Now keep running the following command until you see a value for EXTERNAL-IP!"
+
+echo '$ kubectl get deployment tensorflask'
 
 # Interesting commands
 
