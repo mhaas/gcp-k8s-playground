af45c377a032b85d94b2604f404738de2af8091d
Michael Haas <michael.haas@computerlinguist.org>
2018-03-18 11:26:16 +0100

    Add error handling and proper image name to build-cloud-container-builder.sh

--- build-cloud-container-builder.sh
+++ build-cloud-container-builder.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+function handle_error() {
+    echo "Error occured on line $(caller). Exiting!" 1>&2
+    exit 1
+}
+
+trap handle_error ERR
+
 source common.sh
 
-gcloud container builds submit --tag gcr.io/${PROJECT_ID}/tensorflask .
+gcloud container builds submit --tag ${IMAGE_NAME} .
 
